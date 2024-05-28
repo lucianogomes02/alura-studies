05e8443cfcd1ab716984dbc9dcbdce31657e62eb
Luciano Gomes <lucianogvda02@gmail.com>
2024-05-28 00:35:00 -0300

    refactor: order completed tasks

--- src/components/Tarefas/index.tsx
+++ src/components/Tarefas/index.tsx
@@ -10,11 +10,24 @@ interface Props {
 }
 
 export default function Tarefas({ tarefas, selecionarTarefa, cronometroAtivo }: Props) {
-    return(
+    const ordenarTarefasConcluidas = [...tarefas].sort((tarefaAtual, tarefaAnterior) => {
+        if (tarefaAtual.completado === true && tarefaAnterior.completado === true) {
+            return 0; 
+        }
+        if (tarefaAtual.completado === true) {
+            return -1;
+        }
+        if (tarefaAnterior.completado === true) {
+            return 1;
+        }
+        return 0;
+    });
+
+    return (
         <aside className={style.listaTarefas}>
-            <h2> Tarefas do Dia</h2>
+            <h2>Tarefas do Dia</h2>
             <ul>
-                {tarefas.map((tarefa) => (
+                {ordenarTarefasConcluidas.map((tarefa) => (
                     <Tarefa
                         selecionarTarefa={selecionarTarefa}
                         key={tarefa.id}
@@ -24,5 +37,5 @@ export default function Tarefas({ tarefas, selecionarTarefa, cronometroAtivo }: 
                 ))}
             </ul>
         </aside>
-    )
+    );
 }
